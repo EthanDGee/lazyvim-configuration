7602e709e8d2e445b872e6efa85b491660ed8400
EthanDGee <workethangee@gmail.com>
2025-09-23 13:59:07 -0600

    Initial config with the commands/line behavious slightly changed

---
+++ README.md
@@ -0,0 +1,3 @@
+# 💤 LazyVim
+
+My personal LazyVim config
---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Line Number Settings
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+-- Add end of line editing
+vim.opt.virtualedit = "onemore" -- cursor at end of line
+vim.opt.whichwrap:append("<,>,[,],h,l") -- enable cursor to wrap around endings of lines
