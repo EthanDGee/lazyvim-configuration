82ca281de9b90545e47560fa892473420412607f
EthanDGee <workethangee@gmail.com>
2025-12-24 22:12:53 -0700

    Set up latex to have line wrapping for better viewing while editing

---
+++ after/ftplugin/tex.lua
@@ -0,0 +1,8 @@
+-- Enable soft wrapping
+vim.opt_local.wrap = true
+
+-- Wrap at words (don't split words in the middle)
+vim.opt_local.linebreak = true
+
+-- Preserve indentation when wrapping (optional but recommended)
+vim.opt_local.breakindent = true
