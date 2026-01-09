98a52c62f4f4c85c1937122dabb2a0c3e608c610
EthanDGee <workethangee@gmail.com>
2026-01-08 23:03:04 -0700

    Changed pdf previewer for Vimtex to zathura

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,8 +1,7 @@
 return {
   "lervag/vimtex",
-  init = function()
-    vim.g.vimtex_view_method = "general"
-    vim.g.vimtex_view_general_viewer = "firefox"
-    vim.g.vimtex_view_general_options = "@pdf" -- could be @pdf
+  ft = { "tex" },
+  config = function()
+    vim.g.vimtex_view_method = "zathura"
   end,
 }
