0ee1916c96dd89b66d1f6d8044b3d567900be46d
EthanDGee <workethangee@gmail.com>
2026-01-19 07:48:55 +0100

    Updated the commands for pdfviewing to use the buffer
    Changed ftplugin to pdfview instead of pdf view, and passed int the
    buffer = true option so that it works within the buffer

--- after/ftplugin/pdfview.lua
+++ after/ftplugin/pdfview.lua
@@ -3,13 +3,13 @@ vim.keymap.set(
   "n",
   "<leader>cj",
   "<cmd>:lua require('pdfview.renderer').next_page()<CR>",
-  { desc = "PDFview: Next page" }
+  { buffer = true, desc = "PDFview: Next page" }
 )
 
 -- Navigate to the previous page in the PDF
 vim.keymap.set(
   "n",
-  "<leader>kk",
+  "<leader>ck",
   "<cmd>:lua require('pdfview.renderer').previous_page()<CR>",
-  { desc = "PDFview: Previous page" }
+  { buffer = true, desc = "PDFview: Previous page" }
 )
