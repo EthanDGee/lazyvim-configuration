4dbbaa28575c1bb1d58096e2eaeb2cb923cb0bda
EthanDGee <workethangee@gmail.com>
2026-02-07 19:38:42 -0700

    feat: background is now semi-transparent

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- set background to be transparent
+vim.cmd([[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]])
