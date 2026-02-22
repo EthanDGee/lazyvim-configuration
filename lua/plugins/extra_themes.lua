8240f171f4dff41585efb427dfa26852a2940d9c
EthanDGee <workethangee@gmail.com>
2026-02-22 11:46:30 -0700

    feat: added extra color schemes and changed default theme
    default theme is now tokyo moon
    
    Added the following extra themese
    - Gruvbox
    - Kanagawa
    - Rose-Pine
    - Everforest
    - NightFox
    - Cyberdream

---
+++ lua/plugins/extra_themes.lua
@@ -0,0 +1,18 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Dark / High Contrast
+  { "rebelot/kanagawa.nvim" },
+
+  -- low-fi, cozy, minimalist aesthetic.
+  { "rose-pine/neovim", name = "rose-pine" },
+
+  -- nature-based green and earthy tones.
+  { "sainnhe/everforest" },
+
+  -- sharp, and modern (includes variants like 'Nordfox' and 'Terafox')
+  { "EdenEast/nightfox.nvim" },
+
+  -- High-contrast, futuristic neon theme.
+  { "scottmckendry/cyberdream.nvim" },
+}
