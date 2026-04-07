e11823621aca6b9e3b83eaedf25637886341005c
EthanDGee <workethangee@gmail.com>
2026-04-06 23:50:24 -0600

    feat: added random splash text to the dashboard
    This covers a variety of topics from language specific jokes, to
    references to the honky-vim bird theming, to some race theming. These
    make the entry into the dashboard a fair bit more fun as it adds a bit
    of randomness.

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,3 +1,36 @@
+BASE_HEADER = [[
+         🪿 🐤   🐤     🐤 🐤                                           
+██╗  ██╗ ██████╗ ███╗   ██╗██╗  ██╗██╗   ██╗     ██╗   ██╗██╗███╗   ███╗
+██║  ██║██╔═══██╗████╗  ██║██║ ██╔╝╚██╗ ██╔╝     ██║   ██║██║████╗ ████║
+███████║██║   ██║██╔██╗ ██║█████╔╝  ╚████╔╝█████╗██║   ██║██║██╔████╔██║
+██╔══██║██║   ██║██║╚██╗██║██╔═██╗   ╚██╔╝ ╚════╝╚██╗ ██╔╝██║██║╚██╔╝██║
+██║  ██║╚██████╔╝██║ ╚████║██║  ██╗   ██║         ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝          ╚═══╝  ╚═╝╚═╝     ╚═╝]]
+
+SPLASH_TEXT = {
+  -- Honky Vim Themed
+  [[                           Property of HexCorp                          ]],
+  [[   🇨🇦 🇨🇦 🇨🇦  Proudly Sponsored by Candian Geese Everywhere  🇨🇦 🇨🇦 🇨🇦    ]],
+  [[󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 󰇥 ]],
+  [[🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 🦆 ]],
+  [[                                    (Get it "honky" like the "tonk")    ]],
+  [[                                                 HONK HONK HONK HONK    ]],
+  -- Racing Related
+  [[     DO DO DOO DOOO MAX VERSTAPPEN!  DO DO DOO DOOO MAX VERSTAPPEN!     ]],
+  [[                                                VROOOM VROOOM VROOOM    ]],
+  [[                                                   󰱧  󰱧  󰱧  󰱧  󰱧  󰱧     ]],
+  -- Neovim Related
+  [[                                                     (by the way)       ]],
+  [[                                                             (:wq)      ]],
+  -- Language specific
+  -- [[                  󰌛           󰨊                     ]],
+  [[                       Rewriting to use PostScript                      ]],
+  [[                         Have you heard of Rust?                        ]],
+}
+
+-- change the seed to ensure a random selection
+math.randomseed(os.time())
+
 return {
   {
     "folke/snacks.nvim",
@@ -5,15 +38,7 @@ return {
       dashboard = {
         preset = {
           -- Paste your custom ASCII art inside the [[ ]]
-          header = [[
-         🪿 🐤   🐤     🐤 🐤                                           
-██╗  ██╗ ██████╗ ███╗   ██╗██╗  ██╗██╗   ██╗     ██╗   ██╗██╗███╗   ███╗
-██║  ██║██╔═══██╗████╗  ██║██║ ██╔╝╚██╗ ██╔╝     ██║   ██║██║████╗ ████║
-███████║██║   ██║██╔██╗ ██║█████╔╝  ╚████╔╝█████╗██║   ██║██║██╔████╔██║
-██╔══██║██║   ██║██║╚██╗██║██╔═██╗   ╚██╔╝ ╚════╝╚██╗ ██╔╝██║██║╚██╔╝██║
-██║  ██║╚██████╔╝██║ ╚████║██║  ██╗   ██║         ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝          ╚═══╝  ╚═╝╚═╝     ╚═╝
-                                                    (by the way)        ]],
+          header = BASE_HEADER .. "\n" .. SPLASH_TEXT[math.random(#SPLASH_TEXT)],
         },
       },
     },
