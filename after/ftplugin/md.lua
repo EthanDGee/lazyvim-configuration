c7673ba6650480d4b2d262f95b67ad675c334c52
EthanDGee <workethangee@gmail.com>
2026-07-11 11:50:28 -0600

    feat(markdown): updated styling to better handle reading README.md s

--- after/ftplugin/md.lua
+++ after/ftplugin/md.lua
@@ -1,3 +1,7 @@
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
+
+vim.opt_local.wrap = true -- Enable soft wrapping
+vim.opt_local.linebreak = true -- Wrap at words (don't split words in the middle)
+vim.opt_local.breakindent = true -- Preserve indentation when wrapping
